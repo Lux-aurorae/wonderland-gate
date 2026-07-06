2404f6b42852675d1f4f0f7c9d9cb93438d4d521
Lux-aurorae <sherlock216@naver.com>
2026-07-06 17:05:33 +0900

    final-commit

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
 let computernum = 0
 let play button = document.getElementById("play-button");
-let userinput = document.getElementById(
-    user-input");
+console.log(play)
 
-playbutton.addeventlistener("click",play);
+playbutton.addeventlistener("click",play)
 
 function pickrandomnum(){
     computernum = Math.floor(Math.random()*100+1);
     console.log("정답",computerNum);
 
 }
-pick
 
-function paly() {
-    let uservalue = userinput.ariaValueMax;
-    if(uservalue < computernum) {
-        consloe.log("Up!!!")
-    } else if (uservalue > computerNum) {
-        console.log("Down!!");
-    } else { console.log("맞추셨습니다!!!")};
+funtion pickrandomnum(){
+    computernum = Math.floor(Math.random()*100+1);
+    console.log("정답",computerNum);
 }
-}
-pickrandomnum();
+
+pcikrandomnum();
