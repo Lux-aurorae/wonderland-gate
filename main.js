1327d32c75f3abe092fca1a346c1a6ffea37ad97
Lux-aurorae <sherlock216@naver.com>
2026-07-06 17:21:51 +0900

    fianl-commit

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-let computernum = 0
-let play button = document.getElementById("play-button");
-console.log(play)
+let computernum = 0;
+let playButton = document.getElementById("play-button");
+let userInput = document.getElementById("user-input");
 
-playbutton.addeventlistener("click",play)
+playbutton.addeventlistener("click",play);
 
 function pickrandomnum(){
     computernum = Math.floor(Math.random()*100+1);
     console.log("정답",computerNum);
-
 }
 
-funtion pickrandomnum(){
-    computernum = Math.floor(Math.random()*100+1);
-    console.log("정답",computerNum);
-}
+function play(){
+  let uservalue = userInput.value;
+  console.log(uservalue);
 
-pcikrandomnum();
+pickrandomnum();
